09e2e9e13aa55d264fd9252a19365f05411e4f37
kencas <kcjojo8@gmail.com>
2019-12-09 20:04:57 +0100

    lastest fix

--- src/pilot/routes/customers.js
+++ src/pilot/routes/customers.js
@@ -69,6 +69,22 @@ router.post('/createOrder',(req, res, next) => {
 
 });
 
+router.post('/initfunding',(req, res, next) => {
+
+    
+    customerService.initfunding(req.body)
+    .then(result => {
+        console.log(result),
+        res.status(200).json(result);
+    })
+    .catch(err => {
+            
+         console.log(err),
+        res.status(500).json(err);
+    });
+
+});
+
 
 
 router.get('/transactions',async(req, res, next) => {
