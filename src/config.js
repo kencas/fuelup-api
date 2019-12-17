330b355200eb3ea0360a23a786ed09a4513e45c6
kencas <kcjojo8@gmail.com>
2019-12-17 13:26:13 +0100

    push

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    "database" : "mongodb://localhost:27017/fuelup", 
+    //"database" : "mongodb://localhost:27017/fuelup", 
     "database" : "mongodb://fuelup:fuelup4real@ds161751.mlab.com:61751/fuelup",
     "port" : process.env.PORT || 3000,
     "secretKey" : "66&&&888^^TTggg4$$$55g"
